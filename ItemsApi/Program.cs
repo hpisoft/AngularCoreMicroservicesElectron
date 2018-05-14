128c2f6dc6f730bac51a43ffd851e9a3642e68e0
Peter Horn <hpisoft.com@gmail.com>
2018-05-14 22:47:01 +0200

    Added logging Added .env file to control environment variables

--- ItemsApi/Program.cs
+++ ItemsApi/Program.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using dotenv.net;
 
 namespace ItemsApi
 {
@@ -22,10 +24,13 @@ namespace ItemsApi
             var config = new ConfigurationBuilder()
                             .AddCommandLine(args)
                             .Build();
-
+            DotEnv.Config(false, ".env", Encoding.Unicode);
+            var serverurls = string.Format("http://localhost:{0}", Environment.GetEnvironmentVariable("PORT"));
             return WebHost.CreateDefaultBuilder(args)
                 .UseConfiguration(config)
+                .ConfigureLogging(f => f.AddConsole())
                 .UseStartup<Startup>()
+                .UseUrls(serverurls)
                 .Build();
         }
     }
--- ItemsApi/Startup.cs
+++ ItemsApi/Startup.cs
@@ -28,8 +28,11 @@ namespace ItemsApi
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+	        loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+	        loggerFactory.AddDebug();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
